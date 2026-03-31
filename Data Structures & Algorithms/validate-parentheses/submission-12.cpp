020e91a81eaee16c538f61894f677a13783c790c
Lakshmi Narayanan R <45351359+lechu24@users.noreply.github.com>
2026-03-31 23:11:32 +0530

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> res;
+
+        for (const auto& x : s) {
+            if (x == '{' || x == '[' || x == '(') {
+                res.push(x);
+            } else {
+                if (res.empty()) return false;
+
+                char t = res.top();
+                if ((x == '}' && t == '{') ||
+                    (x == ']' && t == '[') ||
+                    (x == ')' && t == '(')) {
+                    res.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return res.empty();
+    }
+};
